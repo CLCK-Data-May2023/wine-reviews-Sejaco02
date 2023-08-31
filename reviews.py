06848af9ea9949f6efa0648a17ad8d971192be13
Seth Jacobs <sejaco02@gmail.com>
2023-08-31 18:00:54 -0400

    updated with py script, data folder/file and venv

--- reviews.py
+++ reviews.py
@@ -1,2 +1,13 @@
-# add your code here
+import pandas as pd
 
+reviews = pd.read_csv("data/winemag-data-130k-v2.csv.zip")
+summary_rev = reviews.groupby("country").points.agg(["count", "mean"]).round(1)
+summary_rev = summary_rev.rename(columns = {"mean":"points"})
+
+summary_rev.to_csv("data/reviews-per-country.csv")
+
+
+
+print(reviews.head())
+print(summary_rev.head())
+print(summary_rev.info())
